ca1e4586eb0563e6b0dbc56970be6a0287d35fef
jfitz02 <jfitz02@outlook.com>
2021-02-28 10:13:50 +0000

    Added ALLOWED_HOSTS

--- mealPlanner/mealPlanner/settings.py
+++ mealPlanner/mealPlanner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mrw=1i^j1t%jt&q*)dqqe=94z7#e=h%8^y!mumo*t7*7m29c=7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
