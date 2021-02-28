4c41ee5d91b7a3084368b6d0ce0754683f57d37a
jfitz02 <jfitz02@outlook.com>
2021-02-28 11:01:16 +0000

    Cleaned up redundant code

--- mealPlanner/polls/models.py
+++ mealPlanner/polls/models.py
@@ -5,13 +5,6 @@ from django.utils import timezone
 
 # Create your models here.
 
-# class MyUser(models.Model):
-#     username = models.CharField(max_length=24)
-#     password = models.CharField(max_length=30)
-
-#     def __str__(self):
-#         return self.username
-
 class Ingredient(models.Model):
     ingredient_name = models.CharField(max_length=50)
 
@@ -27,7 +20,6 @@ class Unit(models.Model):
     
 
 class Meal(models.Model):
-    # user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     meal_name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -42,23 +34,3 @@ class MealIngredient(models.Model):
 
     def __str__(self):
         return str(self.meal) + " "+str(self.ingredient)+" "+str(self.quantity)+str(self.unit)
-
-
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-
-#     def was_published_recently(self):
-#         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-#     def __str__(self):
-#         return self.question_text
-
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
-
-#     def __str__(self):
-#         return self.choice_text
--- mealPlanner/polls/views.py
+++ mealPlanner/polls/views.py
@@ -1,88 +1,3 @@
-# from django.http import HttpResponse, HttpResponseRedirect
-
-# from django.shortcuts import render, get_object_or_404
-
-# from django.urls import reverse
-
-# from .models import Question, Choice
- 
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
-
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
-
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-#     except (KeyError, Choice.DoesNotExist):
-#         # Redisplay the question voting form.
-#         return render(request, 'polls/detail.html', {
-#             'question': question,
-#             'error_message': "You didn't select a choice.",
-#         })
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-    
-# from django.http import HttpResponseRedirect
-# from django.shortcuts import get_object_or_404, render
-# from django.urls import reverse
-# from django.views import generic
-
-# from .models import Meals, Ingredients
-
-
-# class IndexView(generic.ListView):
-#     template_name = 'polls/index.html'
-#     context_object_name = 'latest_question_list'
-
-#     def get_queryset(self):
-#         """Return the last five published questions."""
-#         return Question.objects.order_by('-pub_date')[:5]
-
-
-# class DetailView(generic.DetailView):
-#     model = Question
-#     template_name = 'polls/detail.html'
-
-
-# class ResultsView(generic.DetailView):
-#     model = Question
-#     template_name = 'polls/results.html'
-
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-#     except (KeyError, Choice.DoesNotExist):
-#         # Redisplay the question voting form.
-#         return render(request, 'polls/detail.html', {
-#             'question': question,
-#             'error_message': "You didn't select a choice.",
-#         })
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-   
-
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -91,25 +6,6 @@ from django.utils.datastructures import MultiValueDictKeyError
 
 from .models import Meal, Ingredient, MealIngredient, Unit
 from .generate_list import generate
-
-# class UserView(generic.ListView):
-#     try:
-#         name = request.POST['newmeal']
-#         meal = Meal(meal_name=name)
-#         meal.save()
-#     except:
-#         pass
-#     template_name = 'polls/index.html'
-#     context_object_name = 'alphabetical_meals_list'
-
-#     def get_queryset(self):
-#         """Return the last five published questions."""
-#         print(Meal.objects.order_by('meal_name'))
-#         return Meal.objects.order_by('meal_name')
-
-
-# def login_view(request):
-#     return render(request, 'polls/login.html')
 
 def user_view(request):
     if request.method == "POST":
@@ -200,7 +96,3 @@ def meal_view(request, meal_id):
         raise Http404("There is no such meal!")
 
     return render(request, 'polls/detail.html', {'meal':meal, 'mi':ingredients, 'ing':alling, 'units':allunits})
-
-
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
